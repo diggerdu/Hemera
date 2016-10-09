3930b37eca54bb0da31405a1d9d01fe793e83437
diggerdu <diggerdu@hotmail.com>
2016-10-09 16:30:29 +0800

    prototype

---
+++ prototype/build/CMakeFiles/prototype.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prototype.dir/main.cpp.o"
+  "prototype.pdb"
+  "prototype"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prototype.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- prototype/main.cpp
+++ prototype/main.cpp
@@ -8,5 +8,5 @@ int main()
     topology.push_back(3);
     topology.push_back(2);
     topology.push_back(1);
-    
+    std::cout<<"hello word"<<std::endl;
 }
