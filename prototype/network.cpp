362208fb4ccec7b89df5aa34c09dbd83479c40b4
diggerdu <diggerdu@hotmail.com>
2016-11-30 20:00:13 +0800

    breakpoint

--- prototype/network.cpp
+++ prototype/network.cpp
@@ -1,23 +1,47 @@
 #include "network_pre.h"
 
-Node::Node(double(*activation_function)(double sum)):activate(activation_function)
+
+
+
+LayerInfo::LayerInfo::LayerInfo(double (*activation_function )(double sum), size_t NumNode)
 {
+    activate = activation_function;
+    numnodes = NumNode;
+};
+
+
+Node::Node(const std::vector<double>& Synapse, double(*activation_function)(double sum)):activate(activation_function)
+{
+    synapse = Synapse;
     OutputVal = 0;
 };
 
 void Node::feedForward(const std::vector<double>& InputVals)
 {
     double sum = 0;
-    assert(InputVals.size() == Synapse.size());
-    for (unsigned i = 0; i < Synapse.size(); i++)
-        sum += InputVals[i]*Synapse[i];
+    assert(InputVals.size() == synapse.size());
+    for (unsigned i = 0; i < synapse.size(); i++)
+        sum += InputVals[i] * synapse[i];
     setOutputVal(activate(sum));
 };
 
 
 
 
+Layer::Layer(const std::vector<std::vector<double> >& Synapses, size_t NumNode, double(*activation_function)(double sum))
+{
+        assert(Synapses.size() == NumNode);
+        for (unsigned i = 0; i < NumNode; i++)
+            nodes.push_back(Node(Synapses[i], activation_function));
+};
 
+
+void Layer::setOutputVals(std::vector<double> inputVals)
+{
+    assert(nodes.size() == inputVals.size());
+    for (size_t i = 0; i < inputVals.size(); i++)
+        nodes[i].setOutputVal(inputVals[i]);
+};
 
 std::vector<double> Layer::getOutputVals()
 {
@@ -33,4 +57,30 @@ void Layer::feedForward(Layer& PrevLayer)
     PrevOutputs = PrevLayer.getOutputVals();
     for (unsigned i = 0; i < nodes.size(); i++)
         nodes[i].feedForward(PrevOutputs);
+};
+
+
+
+Net::Net(const std::vector<LayerInfo>& Topo, const std::vector<std::vector<std::vector<double> > >& Synapses)
+{
+    for (unsigned i = 0; i < Topo.size(); i++)
+        layers.push_back(Layer(Synapses[i], Topo[i].numnodes, Topo[i].activate));
+};
+
+void Net::setInputVals(const std::vector<double> & InputVals)
+{
+    assert(layers.size()>1);
+    layers[1].setOutputVals(InputVals);
+};
+
+void Net::feedForward()
+{
+    assert(layers.size() > 1);
+    for (unsigned i = 1; i < layers.size(); i++)
+        layers[i].feedForward(layers[i-1]);
+};
+
+std::vector<double> Net::getOutputVals()
+{
+    return layers[layers.size()-1].getOutputVals();
 };
--- prototype/network_pre.h
+++ prototype/network_pre.h
@@ -12,16 +12,26 @@ class Node;
 class Layer;
 class Net;
 
+
+class LayerInfo
+{
+public:
+    LayerInfo(double(*activation_function)(double sum), size_t NumNode);
+    unsigned numnodes;
+    double (*activate)(double sum);
+};
+
+
 class Node
 {
 public:
-    Node(double(*activation_function)(double sum));
+    Node(const std::vector<double>& Synapse, double(*activation_function)(double sum));
     void feedForward(const std::vector<double>& InputVals);
     void setOutputVal(const double Val) {OutputVal = Val;};
     double getOutputVal() {return OutputVal;};
 private:
     double OutputVal;
-    std::vector<double> Synapse;
+    std::vector<double> synapse;
     double (*activate)(double sum);
 };
 
@@ -29,13 +39,26 @@ private:
 class Layer
 {
 public:
-    Layer(const std::vector<std::vector<double> > Synapses);
+    Layer(const std::vector<std::vector<double> >& Synapses, size_t NumNode, double(*activation_function)(double sum));
     void feedForward(Layer& Prevlayer);
+    void setOutputVals(std::vector<double> inputVals);
     std::vector<double> getOutputVals();
 private:
     std::vector<Node> nodes;
 };
 
+
+
+class Net
+{
+public:
+    Net(const std::vector<LayerInfo>& Topo, const std::vector<std::vector<std::vector<double> > >& Synapses);
+    void setInputVals(const std::vector<double> & InputVals);
+    void feedForward();
+    std::vector<double> getOutputVals();
+private:
+    std::vector<Layer> layers;
+};
 
 /*range from 0 to 1
  */
